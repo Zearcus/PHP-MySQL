281acfa241989dc5b7ecffb96b9256cee8ae955a
Zearcus <84633381+Zearcus@users.noreply.github.com>
2021-11-29 12:15:14 +0100

    modifs

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once "config.php"; ?>
 <!DOCTYPE html>
  <html lang="fr">
     <head>
@@ -30,7 +31,14 @@
        <div class="col s12 l2 green darken-1 z-depth-5"><p><a class="blue-text text-lighten-2" href="project1.php">La BulldoCorp Ltd.®</a></p></div>
        <div class="col s12 l2 green darken-2 z-depth-5"><p><a class="blue-text text-lighten-2" href="project2.php">Carré Rouge en Python</a></p></div>
        <div class="col s12 l2 green darken-3 z-depth-5"><p><a class="blue-text text-lighten-1" href="project3.php">I.O.T Système Domotique</a></p></div>
-       <div class="col s12 l2 green darken-4 z-depth-5"><p><a class="blue-text text-lighten-2" href="connexion.php">Se connecter</a></p><a class="btn-small" id="quack">Click here</a></div>
+       <?php if (isset($_SESSION['user'])): ?>
+          <?php if ($_SESSION['user']['is_admin']==1): ?>
+                      <div class="col s12 l2 green darken-4 z-depth-5"><p><a class="blue-text text-lighten-2" href="admin.php">Admin</a></p><a class="btn-small" id="quack">Click here</a></div>
+            <?php endif; ?>
+          <div class="col s12 l2 green darken-4 z-depth-5"><p><a class="blue-text text-lighten-2" href="logout.php">Se déconnecter</a></p><a class="btn-small" id="quack">Click here</a></div>
+        <?php else: ?>
+          <div class="col s12 l2 green darken-4 z-depth-5"><p><a class="blue-text text-lighten-2" href="connexion.php">Se connecter</a></p><a class="btn-small" id="quack">Click here</a></div>
+        <?php endif; ?>
      </div>
 
      <div class="row title_1">
@@ -154,4 +162,4 @@
      <script type="text/javascript" src="js/script.js"></script>
 
     </body>
-  </html>
+  </html> 
--- login.php
+++ login.php
@@ -7,10 +7,12 @@ $pre->execute();
 $user = current($pre->fetchAll(PDO::FETCH_ASSOC));//current prend la première ligne du tableau
 if(empty($user)){ //vérifie si le resultat est vide !
      //non connecté
-     echo "Utilisateur ou mot de passe incorrect !";
+      header('Location:connexion.php');
 }else{
      $_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
+     if($_SESSION['user']['is_admin']==1){
+          header('Location:admin.php');
+     }
+     header('Location:index.php');
 }
-
-header('Location:index.php');//on le redirige sur la page d'accueil du site !
 ?>
