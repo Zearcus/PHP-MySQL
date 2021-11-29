0b81977e1908880f7b9f0865817c65e879bb53b1
ThomasJaffe <92303180+ThomasJaffe@users.noreply.github.com>
2021-11-29 11:56:32 +0100

    fixed nyancat & quack

--- js/inscription.js
+++ js/inscription.js
@@ -1,3 +1,21 @@
+// Bouton Nyan Cat//
+
+document.getElementById("cat").onclick = function() {Functioncat()};
+
+function Functioncat() {
+  var audiocat = new Audio('audio/nyancat.mp3');
+  audiocat.play();
+}
+
+// Bouton quack //
+
+document.getElementById("quack").onclick = function() {Functionquack()};
+
+function Functionquack() {
+  var audioquack = new Audio('audio/quack.mp3');
+  audioquack.play();
+}
+
 // Text Contact //
 $(document).ready(function () {
     $('input#input_text, textarea#textarea2').characterCounter();
--- js/project1.js
+++ js/project1.js
@@ -1,3 +1,21 @@
+// Bouton Nyan Cat//
+
+document.getElementById("cat").onclick = function() {Functioncat()};
+
+function Functioncat() {
+  var audiocat = new Audio('audio/nyancat.mp3');
+  audiocat.play();
+}
+
+// Bouton quack //
+
+document.getElementById("quack").onclick = function() {Functionquack()};
+
+function Functionquack() {
+  var audioquack = new Audio('audio/quack.mp3');
+  audioquack.play();
+}
+
 // Parallax //
 
 $(document).ready(function () {
