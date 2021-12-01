1bd467fa44175c0cf0efc6450f320bb2e68a64d0
ThomasJaffe <92303180+ThomasJaffe@users.noreply.github.com>
2021-12-01 14:55:00 +0100

    added a : in admin panel

--- admin.php
+++ admin.php
@@ -5,13 +5,13 @@
   </head>
   <body>
    <?php require "menu.php"; ?>
-   <h1>Liste des utilisateurs</h1>
+   <h1>Liste des utilisateurs :</h1>
    <?php
-   $sql = "SELECT * FROM user"; 
-   $pre = $pdo->prepare($sql); 
+   $sql = "SELECT * FROM user";
+   $pre = $pdo->prepare($sql);
    $pre->execute();
    $data = $pre->fetchAll(PDO::FETCH_ASSOC);
-   
+
    foreach($data as $user){ ?>
    <div class="bloc_user">
      <h2><?php echo $user['first_name']." ".$user['last_name'] ?></h2>
