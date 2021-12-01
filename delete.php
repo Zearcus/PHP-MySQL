6e3588fc73919157c49c9f2693737c1eefc09c70
Zearcus <84633381+Zearcus@users.noreply.github.com>
2021-12-01 15:53:57 +0100

    modifs

--- delete.php
+++ delete.php
@@ -1,9 +1,11 @@
-#delete.php
+
 <?php
 require_once "config.php";
 
     $sql = "DELETE FROM user WHERE id =".$_POST['id'];
     $pre = $pdo->prepare($sql); 
     $pre->execute();
+
+    header ("Location:admin.php");
 
 ?>
